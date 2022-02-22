3182368207719a8e12c4a0fcb89c5a915d2a6755
p.gerasimov <gerasimov86@gmail.com>
2022-02-23 00:16:28 +0300

    modal show/hide overlay in js

--- js/script.js
+++ js/script.js
@@ -3,13 +3,16 @@ let ModalWindow = document.querySelector('.modal')
 let ModalButtonClose = document.querySelector('.modal-button__close')
 let ModalAdditionalInfo = document.querySelector('.info-icon')
 let ModalChildInfo = document.querySelector('.child-info__block')
+let ModalOverlay = document.querySelector('.overlay')
 
 ModalButton.onclick = function() {
   ModalWindow.classList.remove('invisible')
+  ModalOverlay.classList.remove('overlay-disabled')
 }
 
 ModalButtonClose.onclick = function() {
   ModalWindow.classList.add('invisible')
+  ModalOverlay.classList.add('overlay-disabled')
 }
 
   ModalAdditionalInfo.addEventListener("mouseover",function() {
